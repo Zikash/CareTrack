0eda407805447bef7f1ab9ce6209ba1d6cc4b1f7
Zikash <unius6ls66@gmail.com>
2025-01-29 23:47:42 +0300

    create frontend base

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <my-header/>
+    <router-view></router-view>
+  </div>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style>
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+</style>
---
+++ frontend/src/Components/UI/ButtonAuth.vue
@@ -0,0 +1,25 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+
+<script>
+    export default {
+        name: 'btn-auth'
+    }
+</script>
+
+
+<style>
+    button {
+        margin-top: 0px;
+        padding-top: 0px;
+        border-radius: 0px 0px 15px 15px;
+        border-top: 0px;
+        background-color: #469C56;
+        min-width: 12px;
+        min-height: 10px;
+    }
+</style>
