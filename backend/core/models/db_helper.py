4e9bf159dccf97fcb4222577a5380e79972136f3
yarik <star.space.24.works@gmail.com>
2025-02-01 10:05:34 -0600

    rework db helper

--- backend/core/models/db_helper.py
+++ backend/core/models/db_helper.py
@@ -1,9 +1,16 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+)
 
 from core.config import settings
 
 
-class DatabaseHelper():
+class DatabaseHelper:
     def __init__(
             self,
             url: str,
@@ -11,27 +18,31 @@ class DatabaseHelper():
             echo_pool: bool = False,
             pool_size: int = 5,
             max_overflow: int = 10,
-    ):
-        self.engine = create_async_engine(
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
             url=url,
             echo=echo,
             echo_pool=echo_pool,
             pool_size=pool_size,
             max_overflow=max_overflow,
         )
-        self.session_factory = async_sessionmaker(
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False,
         )
 
-    async def dispose(self):
+    def get_engine(self):
+        return self.engine
+
+    async def dispose(self) -> None:
         await self.engine.dispose()
 
-    async def session_getter(self):
-        async with self.session_factory as session:
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
             yield session
+
 
 db_helper = DatabaseHelper(
     url=str(settings.db.url),
@@ -39,5 +50,4 @@ db_helper = DatabaseHelper(
     echo_pool=settings.db.echo_pool,
     pool_size=settings.db.pool_size,
     max_overflow=settings.db.max_overflow,
-
 )
