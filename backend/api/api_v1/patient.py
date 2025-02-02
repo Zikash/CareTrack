f497c3767f2c114259cde325f779c219a947f4b9
yarik <star.space.24.works@gmail.com>
2025-02-02 10:20:05 -0600

    add patient router

--- backend/api/api_v1/patient.py
+++ backend/api/api_v1/patient.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.config import settings
 from core.models import db_helper
-from core.schemas.patient import PatientRead
+from core.schemas.patient import PatientRead, PatientCreate
 from crud.patient import get_all_patient
 
 router = APIRouter(
+    prefix=settings.api.v1.patient,
     tags=["Patient"],
 )
 
@@ -15,3 +17,20 @@ async def get_patient(
 ):
     patients = await get_all_patient(session=session)  # Использовать await здесь
     return patients
+
+
+@router.get("/add")
+async def render_patient_add():
+    pass
+'''
+@router.post("/add", response_model=list[PatientCreate])
+async def render_patient_add(
+    session: AsyncSession = Depends(db_helper.session_getter),
+
+):
+    new_patient = PatientCreate(
+        name=name
+    )
+    session.add(new_patient)
+    session.commit()
+'''
