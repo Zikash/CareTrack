769fbfe2e6d123dd3517e6827d4db4c4eb9f0470
Matvej Karelin <matvej374@gmail.com>
2025-02-03 01:04:13 +0300

    1a

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,7 +3,12 @@ import { createStore } from "vuex";
 export default createStore({
     state: () => ({
         patient: null,
-        doctor: null,
+        doctor: {
+            Phone: "8 999 999 99-99",
+            Mail: "niPobelov@otvetov.net",
+            Place: 1,
+            Stage: 1
+        },
         administrator: {
             Phone: '8 800 535-35-35',
             Mail: 'nePisat@otveta.net'
